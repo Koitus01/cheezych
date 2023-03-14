a660dc210aca8f68f265131c2338f2e41d477044
Артём Быков <a.bykov@sprinthost.ru>
2023-03-14 14:24:35 +0300

    install phpunit, start to making pawns test classes

---
+++ tests/Unit/Pieces/PawnTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Tests\Unit\Pieces;
+
+class PawnTest extends PhpUnit
+{
+    public function testMovementPattern(): void
+    {
+
+    }
+}
---
+++ tests/Unit/Pieces/Queen.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Tests\Unit\Pieces;
+
+class Queen
+{
+
+}
